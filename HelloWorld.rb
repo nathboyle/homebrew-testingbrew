9c2f48f2362e4744cdbdfc39030366c7b1afac81
nathboyle <nathboyle@gmail.com>
2023-08-03 19:43:54 +0200

    4th commit

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -3,7 +3,6 @@ class HelloWorld < Formula
   homepage "https://github.com/nathboyle/testingbrew"
 
 
-  url "https://bitbucket.org/user/repo/get/HEAD.zip", :using => :curl
   url "https://github.com/nathboyle/testingbrew/blob/main/test.zip", :using => :curl
 
   def install

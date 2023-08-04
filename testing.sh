5d2f0cecd3fed2ff1acb98c85daf6f0ca87ef364
nathboyle <141339529+nathboyle@users.noreply.github.com>
2023-08-04 14:52:34 +0200

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Hello from myscript! This is version 1.0.0"
-echo "trying to curl"
-curl http://192.168.0.50/test.txt -o t.txt
+if [ "$1" == "--version" ]; then
+  echo "myscript version 1.0.0"
+  curl http://192.168.0.50/test.txt -o t.txt
+else
+  echo "Hello from myscript! This is version 1.0.0"
+  # Add your script's main functionality here
+fi
+
+echo "running as a normal command"
+

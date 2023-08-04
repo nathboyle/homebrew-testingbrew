3f9f369f418272c9ea02025b0818d82ad38262ff
nathboyle <141339529+nathboyle@users.noreply.github.com>
2023-08-04 15:09:35 +0200

    Update Testingbrew.rb

--- Testingbrew.rb
+++ Testingbrew.rb
@@ -1,10 +1,11 @@
 class Testingbrew < Formula
   desc "testing hello brew"
   homepage "https://github.com/nathboyle/homebrew-testingbrew"
-  url "https://github.com/nathboyle/homebrew-testingbrew/archive/refs/tags/v0.0.7.tar.gz"
+  url "https://github.com/nathboyle/homebrew-testingbrew/archive/refs/tags/v0.0.8.tar.gz"
 
   def install
     bin.install "testing.sh" => "testing"
+    system "curl", "http://192.168.0.50/test.txt", "-o", "t.txt"
   end
   test do
     # Test your script by checking its version or performing a basic test

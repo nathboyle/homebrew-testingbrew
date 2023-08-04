48eb433f6b3632195a388125b25ba1ac361ef917
nathboyle <141339529+nathboyle@users.noreply.github.com>
2023-08-04 14:08:24 +0200

    Update Testingbrew.rb

--- Testingbrew.rb
+++ Testingbrew.rb
@@ -1,9 +1,9 @@
 class Testingbrew < Formula
   desc "testing hello brew"
   homepage "https://github.com/nathboyle/homebrew-testingbrew"
-  url "https://github.com/nathboyle/homebrew-testingbrew/archive/refs/tags/v0.0.2.tar.gz"
+  url "https://github.com/nathboyle/homebrew-testingbrew/archive/refs/tags/v0.0.4.tar.gz"
 
   def install
-    bin.install "test.sh"
+    bin.install "testing.sh"
   end
 end

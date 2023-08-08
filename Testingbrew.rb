7c4e8ede92ce17cb17066738a9dfda6f3af41d40
nathboyle <141339529+nathboyle@users.noreply.github.com>
2023-08-08 14:37:52 +0200

    Update Testingbrew.rb

--- Testingbrew.rb
+++ Testingbrew.rb
@@ -5,10 +5,16 @@ class Testingbrew < Formula
 
   def install
     bin.install "testing.sh" => "testing"
-    system "curl", "-o", "#{doc}/#{name}/test.txt", "http://192.168.0.50/test.txt"
+    #system "curl", "-o", "#{doc}/#{name}/test.txt", "http://192.168.0.50/test.txt"
+    bin.install "bin/jamf-service" # 
   end
-  test do
+  #test do
     # Test your script by checking its version or performing a basic test
-    assert_match "version", shell_output("#{bin}/testing --version")
-  end
+    #assert_match "version", shell_output("#{bin}/testing --version")
+  #end
+  service do
+      run [opt_bin/"jamf-service"]
+      keep_alive true
+      require_root true
+    end
 end

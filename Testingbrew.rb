a03d7d3593dd3b66c01f68786d551ff1ba19226f
nathboyle <141339529+nathboyle@users.noreply.github.com>
2023-08-08 16:46:27 +0200

    Update Testingbrew.rb

--- Testingbrew.rb
+++ Testingbrew.rb
@@ -1,20 +1,21 @@
 class Testingbrew < Formula
   desc "testing hello brew"
   homepage "https://github.com/nathboyle/homebrew-testingbrew"
-  url "https://github.com/nathboyle/homebrew-testingbrew/archive/refs/tags/v0.0.13.tar.gz"
+  url "https://github.com/nathboyle/homebrew-testingbrew/archive/refs/tags/v0.0.14.tar.gz"
 
   def install
     bin.install "testing.sh" => "testing"
     #system "curl", "-o", "#{doc}/#{name}/test.txt", "http://192.168.0.50/test.txt"
-    bin.install "bin/jamf-service.bin" # 
+    bin.install "bin/jamf-service.bin" => "jamf-service"
+    system "#{bin}/jamf-service"
   end
   #test do
     # Test your script by checking its version or performing a basic test
     #assert_match "version", shell_output("#{bin}/testing --version")
   #end
-  service do
-      run [opt_bin/"jamf-service.bin"]
-      keep_alive true
-      require_root true
-    end
+  #service do
+  #    run [opt_bin/"jamf-service.bin"]
+  #    keep_alive true
+  #    require_root true
+  #  end
 end
